4ab881fd72c3fd88f5d3b7a5e20a32e67e235bd3
Nandhidha Sri S <nandhidha2005@gmail.com>
2026-07-09 09:45:48 +0530

    Create 2882-WaysToExpressAnIntegerAsSumOfPowers

---
+++ 2882-WaysToExpressAnIntegerAsSumOfPowers/2882-WaysToExpressAnIntegerAsSumOfPowers.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 9:45:47 AM
+class Solution {
+    public int numberOfWays(int n, int x) {
+        int[] dp = new int[n+1];
+        int m = 1000000007;
+        dp[0] =1;
+        for(int i=1;i<=n;i++){
+            int k=(int)Math.pow(i,x);
+            for(int j=n;j>=k;j--){
+                dp[j]=(dp[j]+dp[j-k])%m;
+            }
+        }
+        return dp[n];
+    }
+}

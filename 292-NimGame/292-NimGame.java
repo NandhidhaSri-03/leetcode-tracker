b19cc57f600d34e4512da0a4f6ec061c44bff0f0
Nandhidha Sri S <nandhidha2005@gmail.com>
2026-07-09 09:47:23 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,9 @@
+// Last updated: 7/9/2026, 9:47:22 AM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4!=0)
+        return true;
+        else
+        return false;
+    }
+}
